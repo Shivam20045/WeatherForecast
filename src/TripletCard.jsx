02ad824449cd79f3b06887a03deb3dc568056ceb
Delta Student <shivamsinghsuryavanshi11@gmail.com>
2026-06-05 23:33:51 +0530

    blocks names created

--- src/TripletCard.jsx
+++ src/TripletCard.jsx
@@ -4,9 +4,9 @@ import "./TripletCard.css";
 export default function TripletCard() {
   return (
     <div className="Triplet">
-      <DetailCard props={"1st Card."}/>
-      <DetailCard props={"2nd Card"}/>
-      <DetailCard props={"3rd Card"}/>
+      <DetailCard props={"Temperature"}/>
+      <DetailCard props={"Humidity"}/>
+      <DetailCard props={"Tommorow"}/>
     </div>
   );
 }
